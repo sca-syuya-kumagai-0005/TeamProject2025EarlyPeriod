715967314cf29be9e645f8a25d64e4e31c836913
kumagai <sdt22csc0005@edu.sca.ac.jp>
2025-04-30 15:16:44 +0900

    コード整理

--- Assets/kumagai/scripts/CameraMask.cs
+++ Assets/kumagai/scripts/CameraMask.cs
@@ -12,7 +12,7 @@ public class CameraMask : MonoBehaviour
     [SerializeField]private GameObject[]enemies;
     private SpriteRenderer[] enemiesSpriteRenderer;
     [SerializeField]private GameObject photo;
-    [SerializeField]private GameObject album;
+    [SerializeField]private GameObject photoStorage;
     [SerializeField]private GameObject lens;
     private string enemyTag="Enemy";
     private string backGroundTag="BackGround";
@@ -22,11 +22,6 @@ public class CameraMask : MonoBehaviour
     {
         backGround=GameObject.Find(backGroundTag).gameObject;
         enemies=(GameObject.FindGameObjectsWithTag(enemyTag));
-        for(int i=0; i<enemies.Length; i++)
-        {
-            Debug.Log(enemies[i]);
-        }
-       
         mask.transform.position = new Vector3(0, 0, 0);
         Cursor.visible = false;
     }
@@ -34,44 +29,71 @@ public class CameraMask : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            backGround.SetActive(false);
-            lens.SetActive(false);
-            for (int i = 0; i < enemies.Length; i++)
-            {
-                enemies[i].SetActive(false);
-            }
-            photo.transform.position-=transform.position;
-            this.gameObject.SetActive(false);
-        }
-        //ここまではデバッグ用コード
-        if (photo.transform.childCount != 0)
+        if(photo.transform.childCount!=0)
         {
             return;
         }
-        mask.transform.position+=Input.mousePositionDelta*Time.deltaTime*lensSpeed;
-        if(Input.GetMouseButton(1))
+        PointerMove();
+        MakePhoto();
+    }
+
+    /// <summary>
+    /// カーソルの動き　デバッグ用なので仮実装
+    /// </summary>
+    void PointerMove()
+    {
+        mask.transform.position += Input.mousePositionDelta * Time.deltaTime * lensSpeed;
+        if (Input.GetMouseButton(1))
         {
             mask.transform.position = new Vector3(0, 0, 0);
         }
-        if(Input.GetMouseButton(0))
+    }
+
+    /// <summary>
+    /// 写真の切り抜き
+    /// </summary>
+    void MakePhoto()
+    {
+        if (Input.GetMouseButton(0))
         {
-            GameObject tmpThis=Instantiate(this.gameObject,transform.position,Quaternion.identity,photo.transform);
+            GameObject tmpThis = Instantiate(this.gameObject, transform.position, Quaternion.identity, photo.transform);
             tmpThis.GetComponent<CameraMask>().enabled = false;
-            GameObject tmpBackGround=Instantiate(backGround,backGround.transform.position,Quaternion.identity,photo.transform);
-            SpriteRenderer sr= tmpBackGround.GetComponent<SpriteRenderer>();
-            sr.maskInteraction=SpriteMaskInteraction.VisibleInsideMask;
-            for(int i=0;i<enemies.Length;i++)
+            GameObject tmpBackGround = Instantiate(backGround, backGround.transform.position, Quaternion.identity, photo.transform);
+            SpriteRenderer sr = tmpBackGround.GetComponent<SpriteRenderer>();
+            sr.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
+            for (int i = 0; i < enemies.Length; i++)
             {
                 GameObject photoEnemy;
-                photoEnemy=Instantiate(enemies[i], enemies[i].transform.position, Quaternion.identity,photo.transform);
-                sr=photoEnemy.GetComponent<SpriteRenderer>();
+                photoEnemy = Instantiate(enemies[i], enemies[i].transform.position, Quaternion.identity, photo.transform);
+                sr = photoEnemy.GetComponent<SpriteRenderer>();
                 sr.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
             }
+            SavePhoto();
+            DebugFunction();
         }
-
-       
       
+    }
+
+    /// <summary>
+    /// 作成した写真オブジェクトをPhotoStorageにコピー
+    /// </summary>
+    void SavePhoto()
+    {
+        GameObject obj=Instantiate(photo,new Vector3(0,0,0), Quaternion.identity, photoStorage.transform);
+        obj.name="Stage1";
+        photo.SetActive(false);
+    }
+
+    //デバッグ用　背景とかもろもろのActiveをfalseにして見やすくするための関数　
+    void DebugFunction()
+    {
+        backGround.SetActive(false);
+        lens.SetActive(false);
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemies[i].SetActive(false);
+        }
+        photo.transform.position -= transform.position;
+        this.gameObject.SetActive(false);
     }
 }
