0825cbf9a4433ddb28c066bd352b1bc787c8881e
SorakakeruOsushi <sdt2495gp0007@edu.sca.ac.jp>
2025-05-10 15:29:24 +0900

    クリックしたらコライダーを消す・透明化したらオブジェクトをけすプログラムを実装

--- Assets/Nozomi/ClickTest2D.cs
+++ Assets/Nozomi/ClickTest2D.cs
@@ -4,10 +4,18 @@ using System.Collections.Generic;
 
 public class ClickTest2D : MonoBehaviour
 {
+    const float timer=3.0f;
+    float flamecount = 0.0f;
+    [SerializeField] float alphaTimer;
+    bool alphaStart;
+    [SerializeField]Collider2D[] colliders;
+    [SerializeField]SpriteRenderer spriteRenderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        alphaTimer = 1.0f;
+       colliders = GetComponents<Collider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -17,21 +25,37 @@ public class ClickTest2D : MonoBehaviour
 
         // Update is called once per frame
         
-           
-
-            if (Input.GetMouseButtonDown(0))
-            {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
-
-                if (hit2d)
-                {
-                    clickedGameObject = hit2d.transform.gameObject;
-                    Debug.Log(clickedGameObject.name);//ゲームオブジェクトの名前を出力
-                    Destroy(clickedGameObject);//ゲームオブジェクトを破壊
-                }
-
-            }
+        if(alphaStart)
+        {
+            alphaTimer -= Time.deltaTime/timer;
         }
+
+        spriteRenderer.color = new Color(1,1,1, alphaTimer);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            if (hit2d)
+            {
+                alphaStart = true;
+                clickedGameObject = hit2d.transform.gameObject;
+                Debug.Log(clickedGameObject.name);//ゲームオブジェクトの名前を出力
+                for(int i = 0;i < colliders.Length; i++)
+                {
+                    colliders[i].enabled = false;
+                }
+               // Collider.Destroy(clickedGameObject);//ゲームオブジェクトの判定を破壊
+                StartCoroutine(DestroyTimer(clickedGameObject));
+            }
+
+        }
+    }
     
+    IEnumerator DestroyTimer(GameObject obj)
+    {
+        yield return new WaitForSeconds(timer);
+        Destroy(obj);
+    }
 }
