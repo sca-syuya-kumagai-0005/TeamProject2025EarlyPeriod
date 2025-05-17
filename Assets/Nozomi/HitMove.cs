263747089439f96ce84539c324fa66e4e3fd49e2
SorakakeruOsushi <sdt2495gp0007@edu.sca.ac.jp>
2025-05-17 14:49:33 +0900

    当たり判定の移動制限

--- Assets/Nozomi/HitMove.cs
+++ Assets/Nozomi/HitMove.cs
@@ -8,10 +8,15 @@ public class HitMove : MonoBehaviour
     Vector3 rightTopPos;
     Vector3 leftBottomPos;
     public float depth = 10.0f;
+    public float mouseVertical = 2.0f;
+    public float mouseWidth = 2.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
+        rightTopPos = Camera.main.ViewportToWorldPoint(new Vector3(1,1,10.0f));
+        leftBottomPos = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 10.0f));
+
     }
 
     // Update is called once per frame
@@ -22,15 +27,7 @@ public class HitMove : MonoBehaviour
         //スクリーン座標をワールド座標に変換する
         objPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, depth));
         //ワールド座標をゲームオブジェクトの座標に設定する
-        transform.position = objPos;
+        transform.position = new Vector2(Mathf.Clamp(objPos.x, leftBottomPos.x + mouseWidth, rightTopPos.x - mouseWidth), Mathf.Clamp(objPos.y, leftBottomPos.y + mouseVertical, rightTopPos.y - mouseVertical));
 
-        rightTopPos = Camera.main.ViewportToWorldPoint(new Vector3(1,1,10.0f));
-        leftBottomPos = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 10.0f));
-
-        //オブジェクトがスクリーンからはみ出さないようにするやつ
-        /*if (objPos.x > rightTopPos.x) 
-        {
-            objPos.x = rightTopPos.x;
-        }*/
     }
 }
