e29c262e542cbeb672c084d43126a9129e68c9d9
Takahashi-Souma <sdt2495gp0010@edu.sca.ac.jp>
2025-05-17 15:25:14 +0900

    シーンランダム切り替え

--- Assets/souma/stage/stage.cs
+++ Assets/souma/stage/stage.cs
@@ -7,11 +7,11 @@ public class SceneLoopSwitcher : MonoBehaviour
     public string[] sceneNames; // 切り替えたいシーン名を配列で設定
     public float interval = 3f; // 秒数
 
+    private int previousSceneIndex = -1; // 前回選ばれたシーンのインデックス（初期値は-1）
     private int currentSceneIndex = 0;
 
     void Start()
     {
-       
         StartCoroutine(SwitchScenesLoop());
     }
 
@@ -21,14 +21,22 @@ public class SceneLoopSwitcher : MonoBehaviour
         {
             yield return new WaitForSeconds(interval);
 
-            currentSceneIndex = (currentSceneIndex + 1) % sceneNames.Length; // ループ
+            // ランダムにシーンを選ぶ（前回選ばれたシーンを避ける）
+            do
+            {
+                currentSceneIndex = Random.Range(0, sceneNames.Length);
+            } while (currentSceneIndex == previousSceneIndex);
+
+            // 前回選ばれたシーンのインデックスを更新
+            previousSceneIndex = currentSceneIndex;
+
+            // シーンのロード
             SceneManager.LoadScene(sceneNames[currentSceneIndex]);
         }
     }
 
     void Awake()
     {
-        
         if (FindObjectsOfType<SceneLoopSwitcher>().Length > 1)
         {
             Destroy(gameObject);
