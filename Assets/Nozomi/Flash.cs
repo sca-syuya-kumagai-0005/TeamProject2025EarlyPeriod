8615c425bf0c02f1faec7847dbebfa77b431aae9
SorakakeruOsushi <sdt2495gp0007@edu.sca.ac.jp>
2025-05-20 12:32:41 +0900

    フラッシュ実装

--- Assets/Nozomi/Flash.cs
+++ Assets/Nozomi/Flash.cs
@@ -1,22 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class Flash : MonoBehaviour
 {
     bool modeChange;
     bool flashOn;
     float flashTimer;
+    float imageAlpha = 1;
+    float coolTime = 4f;
+    [SerializeField]Image Image;
+    bool getFirst;
+    bool flashCool;
+    [SerializeField] SpriteRenderer spriteRenderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //spriteRenderer = Image.GetComponent<SpriteRenderer>();
         modeChange = false;
-        flashTimer = 0.0f;
+        flashTimer = 120.0f;
+        coolTime = 240.0f;
         flashOn = false;
-        this.gameObject.SetActive(false);
+        Image.gameObject.SetActive(false);
+        getFirst = true;
+        flashCool = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(flashTimer);
         if (Input.GetKey(KeyCode.A))
         {
             modeChange = false;
@@ -27,22 +40,35 @@ public class Flash : MonoBehaviour
         }
         if (modeChange)
         {
-            //フラッシュのテスト（失敗）　デバッグログでチェック
-            /*if (Input.GetMouseButton(0))
+
+            if (Input.GetMouseButton(0))
             {
-                this.gameObject.SetActive(true);
-                flashOn = true;
-            }*/
-        }
-        if (flashOn)
-        {
-            flashTimer++;
-            if (flashTimer >= Time.deltaTime)
-            {
-                this .gameObject.SetActive(false);
-                flashOn=false;
-                flashTimer=0;
+                if (getFirst)
+                {
+                    getFirst = false;
+                    Image.gameObject.SetActive(true);
+                    StartCoroutine(TestCoroutine());
+                }
+ 
             }
+
         }
+        if(flashOn)
+        {
+            imageAlpha -= Time.deltaTime;
+            Image.color = new Color(1,1,1,imageAlpha);
+        }
+    }
+    IEnumerator TestCoroutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+        flashOn = true;
+        yield return new WaitForSeconds(2.0f);
+        Image.gameObject.SetActive(false);
+        Debug.Log("クールタイム終了");
+        flashOn = false;
+        imageAlpha = 1; Image.color = new Color(1, 1, 1, imageAlpha);
+        getFirst = true;
+
     }
 }
