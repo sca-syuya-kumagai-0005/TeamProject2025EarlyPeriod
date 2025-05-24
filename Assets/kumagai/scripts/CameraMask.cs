8539c01b2bf213ae13593b7d127a0aca34758e6e
kumagai <sdt22csc0005@edu.sca.ac.jp>
2025-05-24 15:03:31 +0900

    マスクの統合

--- Assets/kumagai/scripts/CameraMask.cs
+++ Assets/kumagai/scripts/CameraMask.cs
@@ -9,19 +9,19 @@ public class CameraMask :SoundPlayer
     [SerializeField] float lensSpeed;
     private GameObject backGround;
     private SpriteRenderer backGroundSpriteRenderer;
-    [SerializeField]private GameObject[]enemies;
     private SpriteRenderer[] enemiesSpriteRenderer;
     [SerializeField]private GameObject photo;
     [SerializeField]private GameObject photoStorage;
     [SerializeField]private GameObject lens;
     private string enemyTag="Enemy";
     private string backGroundTag="BackGround";
+    [SerializeField] private GameObject hit;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         backGround=GameObject.Find(backGroundTag).gameObject;
-        enemies=(GameObject.FindGameObjectsWithTag(enemyTag));
+      
         mask.transform.position = new Vector3(0, 0, 0);
         Cursor.visible = false;
      
@@ -31,35 +31,33 @@ public class CameraMask :SoundPlayer
     // Update is called once per frame
     void Update()
     {
-        if(photo.transform.childCount!=0)
-        {
-            return;
-        }
+        //if(photo.transform.childCount!=0)
+        //{
+        //    return;
+        //}
         PointerMove();
         MakePhoto();
     }
 
-    /// <summary>
-    /// �J�[�\���̓����@�f�o�b�O�p�Ȃ̂ŉ�����
-    /// </summary>
+
     void PointerMove()
     {
-        mask.transform.position += Input.mousePositionDelta * Time.deltaTime * lensSpeed;
-        if (Input.GetMouseButton(1))
-        {
-            mask.transform.position = new Vector3(0, 0, 0);
-        }
+       transform.position=hit.transform.position;
     }
 
-    /// <summary>
-    /// �ʐ^�̐؂蔲��
-    /// </summary>
+
     void MakePhoto()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
+            Debug.Log("DEBUG");
+            GameObject[] enemies;
+            photo = Instantiate(photo, photoStorage.transform);
+            photo.name = "photo";
+            photo.SetActive(false);
+            enemies = (GameObject.FindGameObjectsWithTag(enemyTag));
             GameObject tmpThis = Instantiate(this.gameObject, transform.position, Quaternion.identity, photo.transform);
-            tmpThis.GetComponent<CameraMask>().enabled = false;
+            //tmpThis.GetComponent<CameraMask>().enabled = false;
             GameObject tmpBackGround = Instantiate(backGround, backGround.transform.position, Quaternion.identity, photo.transform);
             SpriteRenderer sr = tmpBackGround.GetComponent<SpriteRenderer>();
             sr.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
@@ -76,26 +74,23 @@ public class CameraMask :SoundPlayer
       
     }
 
-    /// <summary>
-    /// �쐬�����ʐ^�I�u�W�F�N�g��PhotoStorage�ɃR�s�[
-    /// </summary>
     void SavePhoto()
     {
-        GameObject obj=Instantiate(photo,new Vector3(0,0,0), Quaternion.identity, photoStorage.transform);
-        obj.name="Stage1";
-        photo.SetActive(false);
+        Debug.Log("SavePhoto");
+        //GameObject obj=Instantiate(photo,new Vector3(0,0,0), Quaternion.identity, photoStorage.transform);
+       
     }
 
-    //�f�o�b�O�p�@�w�i�Ƃ���������Active��false�ɂ��Č��₷�����邽�߂̊֐��@
+   
     void DebugFunction()
     {
-        backGround.SetActive(false);
-        lens.SetActive(false);
-        for (int i = 0; i < enemies.Length; i++)
-        {
-            enemies[i].SetActive(false);
-        }
+       // backGround.SetActive(false);
+        //lens.SetActive(false);
+        //for (int i = 0; i < enemies.Length; i++)
+        //{
+        //    enemies[i].SetActive(false);
+        //}
         photo.transform.position -= transform.position;
-        this.gameObject.SetActive(false);
+       // this.gameObject.SetActive(false);
     }
 }
