f4f957c75c4e36551814bfaa6fb917c267f2e86e
Yamazaki <yamazaki-r@sca.ac.jp>
2025-05-24 16:25:38 +0900

    シェーダーグラフの作成

---
+++ Assets/Yamazaki/Script/Animation.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    [SerializeField] private AnimationClip[] animeClip;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
