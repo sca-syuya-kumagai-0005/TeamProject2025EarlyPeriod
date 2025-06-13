5af50776c243cc04af470dc3b53760380199b278
kamata <kenta.kamata@icloud.com>
2025-06-14 01:40:59 +0900

    スクリプトに拡大する場合と拡大しない場合をチェックマークで切り替えるように追加、 エネミーのプレハブ化完了

--- Assets/Nozomi/HitCheakOdokasi.cs
+++ Assets/Nozomi/HitCheakOdokasi.cs
@@ -1,64 +1,72 @@
 using UnityEngine;
 using System.Collections;
 
-//エラー出してるので、使用しない
-//エラー原因：５５行目の(hitManager.Mode == HitManager.modeChange.cameraMode)がnull
-//hitmanagerをインスペクターから導入できない　Enemyがプレハブだから？（要確認）
-
 public class HitCheakOdokasi : MonoBehaviour
 {
     const float timer = 3.0f;
     float alphaTimer;
-    bool alphaStart;//透明化の開始判定フラグ　trueで開始
-    public bool AlphaStart { set { alphaStart = value; } }//alphaStartを他でいじれるようにするセッター。あんまり使わない方がいい
+    bool alphaStart;
+
+    public bool AlphaStart { set { alphaStart = value; } }
+
     Collider2D[] colliders;
     SpriteRenderer spriteRenderer;
     HitManager hitManager;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log(GameObject.Find("Hit").gameObject.GetComponent<HitManager>());
-        hitManager = GameObject.Find("Hit").gameObject.GetComponent<HitManager>();
+        GameObject hitObj = GameObject.Find("Hit");
+        if (hitObj != null)
+        {
+            hitManager = hitObj.GetComponent<HitManager>();
+            if (hitManager == null)
+            {
+                Debug.LogWarning("[HitCheakOdokasi] 'Hit' オブジェクトに HitManager コンポーネントが見つかりません。");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("[HitCheakOdokasi] 'Hit' オブジェクトが見つかりません。");
+        }
+
         alphaStart = false;
         alphaTimer = 1.0f;
         colliders = GetComponents<Collider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //GameObject clickedGameObject;//クリックされたゲームオブジェクトを代入する変数
-
         if (alphaStart)
         {
-            alphaTimer -= Time.deltaTime / timer;//透明化
-            StartCoroutine(DestroyTimer(this.gameObject));//一定時間後に破壊
+            alphaTimer -= Time.deltaTime / timer;
+            StartCoroutine(DestroyTimer(this.gameObject));
         }
+
         for (int i = 0; i < colliders.Length; i++)
         {
-            colliders[i].enabled = !alphaStart;//colliderのオンオフをalphaStartの反対に設定
+            colliders[i].enabled = !alphaStart;
         }
-        spriteRenderer.color = new Color(1, 1, 1, alphaTimer);//透明化を反映
-                                                              //spriteRenderer.color = new Color(1, 1, 1, 0);
 
-
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = new Color(1, 1, 1, alphaTimer);
+        }
     }
-
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        EnemySpriteAnimator esa = GetComponent<EnemySpriteAnimator>();  
-        if(!esa.IsScalingPaused) {return; }
+        EnemySpriteAnimator esa = GetComponent<EnemySpriteAnimator>();
+        if (esa != null && !esa.IsScalingPaused) return;
+
         if (collision.CompareTag("PlayerCamera"))
         {
-            if (hitManager.Mode == HitManager.modeChange.cameraMode)
+            if (hitManager != null && hitManager.Mode == HitManager.modeChange.cameraMode)
             {
                 alphaStart = true;
             }
         }
+
         for (int i = 0; i < colliders.Length; i++)
         {
             colliders[i].enabled = false;
@@ -68,6 +76,9 @@ public class HitCheakOdokasi : MonoBehaviour
     IEnumerator DestroyTimer(GameObject obj)
     {
         yield return new WaitForSeconds(timer);
-        Destroy(obj);
+        if (obj != null)
+        {
+            Destroy(obj);
+        }
     }
 }
