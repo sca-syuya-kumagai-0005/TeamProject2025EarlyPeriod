54b7a92256f773e6b5876bef76d5de173c246c3a
kazuya <>
2025-06-17 12:09:32 +0900

    一連の流れを作成 残りの修正点 ピントの部分のサイズや位置の調整(優先) 名前の入力欄の修正

--- Assets/Kazuya/Scripts/ScoreRanking.cs
+++ Assets/Kazuya/Scripts/ScoreRanking.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,9 +12,16 @@ public class ScoreRanking : MonoBehaviour
     [SerializeField] InputField nameInputField;//名前入力のUI
     [SerializeField] Button subitButton;//名前送信ボタン
     [SerializeField] Text[] rankingText = new Text[10];
+    [SerializeField] GameObject TextObject;
+
+    [Header("各種ボタン")]
+    [SerializeField] GameObject TitkeButton;
+    [SerializeField] GameObject MainGameButton;
 
     List<(string name, int score)> rankingList = new List<(string name, int score)>();
     string filePath;
+
+    bool Rankingfinish = false;
 
     ScoreEvaluation scoreEvaluation;
     void Start()
@@ -39,23 +47,27 @@ public class ScoreRanking : MonoBehaviour
         else
         {
             UpdateRankingDisplay();
+            StartCoroutine(ButtonSetUp());
         }
-
     }
 
 
     void OnSubmitName()
     {
         string playerName = nameInputField.text.Trim();
-        if (string.IsNullOrEmpty(playerName))
+        if (!string.IsNullOrEmpty(playerName))
         {
-            Debug.LogWarning("名前が入力されていません");
-            return;
+            /// 名前の入力
+            InsertScore(playerName, score);
+            SaveRanking();
+            UpdateRankingDisplay();
+            nameInputField.gameObject.SetActive(false);
+            subitButton.gameObject.SetActive(false);
+            StartCoroutine(ButtonSetUp());
         }
-        /// 名前の入力
-        InsertScore(playerName, score);
-        SaveRanking();
-        UpdateRankingDisplay();
+
+        Debug.LogWarning("名前が入力されていません");
+        return;
     } 
 
     /// <summary>
@@ -87,17 +99,18 @@ public class ScoreRanking : MonoBehaviour
     //csvファイルで持ってきたデータをテキストに置換
     void UpdateRankingDisplay()
     {
-        for(int i = 0;i < rankingList.Count; i++)
+        TextObject.SetActive(true);
+        for (int i = 0;i < rankingList.Count; i++)
         {
-            if(i < rankingList.Count)
+            if (i < rankingList.Count)
             {
-                rankingText[i].text = $"{i + 1}位:{rankingList[i].name}-{rankingList[i].score}";
+                rankingText[i].text = $"{i + 1}位    :   {rankingList[i].name} :{rankingList[i].score}";
             }
             else
             {
-                rankingText[i].text = $"{i+1}位; ---";
+                rankingText[i].text = $"{i+1}位; :";
             }
-
+            TextObject.SetActive(true);
         }
 
     }
@@ -138,5 +151,12 @@ public class ScoreRanking : MonoBehaviour
         }
         File.WriteAllLines(filePath,lines,Encoding.UTF8);
         Debug.Log("ランキングの保存");
+    }
+
+    IEnumerator ButtonSetUp()
+    {
+        yield return new WaitForSeconds(1.0f);
+        TitkeButton.SetActive(true);
+        MainGameButton.SetActive(true);
     }
 }
