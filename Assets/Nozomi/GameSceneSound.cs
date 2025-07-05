79582e3eb142b46387b293762f63557daccfea69
SorakakeruOsushi <sdt2495gp0007@edu.sca.ac.jp>
2025-07-05 17:29:24 +0900

    メインゲームのサウンド実装

---
+++ Assets/Nozomi/GameSceneSound.cs
@@ -0,0 +1,20 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameSceneSound : SoundPlayer
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        BGMPlayer("BGM_1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            SEPlayer("SE_6_Camera", false);
+        }
+    }
+}
